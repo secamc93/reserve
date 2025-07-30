7954a7744898e00017bffc960576c27570bda9f0
sebastian camacho <secamc93@gmail.com>
2025-07-30 12:58:12 -0500

    📚 agrgando api key en cros

--- back/central-reserve/internal/infra/primary/http2/middleware/cors.go
+++ back/central-reserve/internal/infra/primary/http2/middleware/cors.go
@@ -10,7 +10,7 @@ func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, Accept, X-Requested-With")
+		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, Accept, X-Requested-With, X-API-Key")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
