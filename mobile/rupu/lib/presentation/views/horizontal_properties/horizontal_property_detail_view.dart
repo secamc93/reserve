0d3056404eb577a62cd32e15eecc9e08e033a7e4
Reales Myles <rclarence123@gmail.com>
2025-10-20 21:24:53 -0500

    Se agrega opción de agregar propiedad y de ver detalle de la propiedad

--- mobile/rupu/lib/presentation/views/horizontal_properties/horizontal_property_detail_view.dart
+++ mobile/rupu/lib/presentation/views/horizontal_properties/horizontal_property_detail_view.dart
@@ -3,24 +3,18 @@ import 'package:get/get.dart';
 
 import 'horizontal_property_detail_controller.dart';
 
-class HorizontalPropertyDetailView extends GetView<HorizontalPropertyDetailController> {
+class HorizontalPropertyDetailView
+    extends GetView<HorizontalPropertyDetailController> {
   static const name = 'horizontal-property-detail';
   final int propertyId;
 
-  HorizontalPropertyDetailView({
-    super.key,
-    required this.propertyId,
-  }) : super(tag: HorizontalPropertyDetailController.tagFor(propertyId));
+  HorizontalPropertyDetailView({super.key, required this.propertyId});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detalle propiedad'),
-      ),
-      body: const Center(
-        child: Text('Detalle propiedad'),
-      ),
+      appBar: AppBar(title: const Text('Detalle propiedad')),
+      body: const Center(child: Text('Detalle propiedad')),
     );
   }
 }
